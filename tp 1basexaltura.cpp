b7fb84554003a750fdac08faa4ac2338989fe7a1
Arcenegui Liz <164919106+EniniVZTA@users.noreply.github.com>
2024-03-25 14:47:20 -0300

    Add files via upload

---
+++ tp 1basexaltura.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int base,altura,superficie;
+	//Entrada
+	cout<<"ingresar altura:"<<endl;
+	cin>>altura;
+	cout<<"inigresar base:"<<endl;
+	cin>>base;
+	
+	//Proceso 
+	superficie=base*altura;
+	
+	//Salida
+	cout<<"el valor de la superficie es:"<<superficie<<endl;
+	
+	return 0;
+}
