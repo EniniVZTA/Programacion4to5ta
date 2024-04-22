890ef3f4038d9796e2cd40d8de738f2625139975
Arcenegui Liz <164919106+EniniVZTA@users.noreply.github.com>
2024-04-22 14:10:00 -0300

    Add files via upload

---
+++ TpN°6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int a,b;
+	cout<< "Digite un numero"<<endl;
+	 cin>> a;
+	 cout<< "Digite otro numero"<<endl;
+	  cin>> b;
+	  if (a>=b)
+	 {
+	 cout<< "El numero mas grande es:"<<a;
+	 }
+	else
+{
+	cout<< "El numero mas grande es:"<<b;
+}
+	
+	
+	return 0;
+	
+}
