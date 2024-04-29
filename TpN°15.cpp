5e3a1e17b087be06b07caf7b6ed6a9a354cfe214
Arcenegui Liz <164919106+EniniVZTA@users.noreply.github.com>
2024-04-29 14:21:24 -0300

    Add files via upload

---
+++ TpN°15.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int i ;
+	int numero;
+	cout<<"Ingrese cantidad de veces a repetir"<<endl;
+	cin>>numero;
+	
+	for (i=1;i <=numero;i++)
+	{
+		cout<<"Hola Mundo :3"<<endl;
+	}
+	
+		return 0;
+	}
