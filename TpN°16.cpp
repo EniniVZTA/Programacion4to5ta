8e71e3b242df8a2673234d0b21e16c5db78e9394
Arcenegui Liz <164919106+EniniVZTA@users.noreply.github.com>
2024-04-29 14:42:22 -0300

    Add files via upload

---
+++ TpN°16.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int i;
+	
+	while (i<=7)
+	{
+		cout<<"Adios Mundo D:"<<i<<endl;
+		 i++;
+	}
+
+		return 0;
+//for( a=0; a < 7; a++ )
+}
